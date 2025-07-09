fcc47712c30f2fee4c4e31e1854f11c3b8bf20f0
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-08 23:41:02 -1000

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ express()
 
 
   
-  .get('/tableinsert', (req, res) => res.render('pages/tableinsert'))
+  .get('/tableinsert', (req, res) => res.render('/tableinsert'))
   .post('/tableinsertsubmit', (req, res) => {
       var brand;
       var model;

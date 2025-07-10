79a569368ca32348475584bd08d78c56f2fef89f
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-09 18:53:58 -1000

    Update exampledialogclientsidescript.js

--- public/exampledialogclientsidescript.js
+++ public/exampledialogclientsidescript.js
@@ -3,6 +3,7 @@ const openButton = document.getElementById("openDialog");
 const closeButton = document.getElementById("closeDialog");
 
 openButton.addEventListener("click", () => {
+  console.log("exampledialog click open");
   dialog.showModal(); // Opens as a modal dialog
 });
 

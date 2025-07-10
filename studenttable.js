54f3691cb774b46b2f5f540b0ee3e8b00d81e508
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-09 22:12:56 -1000

    Update studenttable.js

--- studenttable.js
+++ studenttable.js
@@ -246,6 +246,78 @@ module.exports = {
   },
 
   studenttableupdate3func: function(req, res) {
-      res.send("not that far");
+      var firstname;
+      var lastname;
+      var email;
+      var phoneareacode;
+      var phonenumber;
+      async function connectAndInsert() {    
+                        const client       = new Client(connectobj);
+                        try {
+                          await client.connect();
+                          console.log('INSERT INTO student Connected to PostgreSQL!');
+                          var insertstmt = "INSERT INTO student (FIRSTNAME, LASTNAME, EMAIL, PHONEAREACODE, PHONENUMBER) VALUES ('" + firstname + "', '" + lastname + "', '" + email + "', " + phoneareacode + ", " + phonenumber + ");";
+                          console.log(insertstmt);
+                          const insertRes = await client.query(insertstmt);
+                          var resultstr = 'insertRes = ' + JSON.stringify(insertRes);
+                          res.render('pages/result', {myresults: resultstr} );
+                        } catch (err) {
+                            var badstr = 'INSERT INTO student ERROR = ' + err;
+                            res.render('pages/result', {myresults: badstr} );
+                        } finally {
+                            await client.end();
+                            console.log('INSERT INTO student Disconnected from PostgreSQL.');
+                        }
+      }
+    
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+  
+          if (err)
+          {
+             res.send("studenttableinsertfunc err = " + err);
+          }
+          else
+          { //good
+             console.log("fields = " + JSON.stringify(fields) + "<br/>files = " + JSON.stringify(files));
+             firstname = fields.firstname_name;
+             lastname = fields.lastname_name;
+             email = fields.email_name;
+             phoneareacode = fields.phoneareacode_name;
+             phonenumber = fields.phonenumber_name;
+
+             //validation checking
+             if (phoneareacode < 0 || phoneareacode > 999)
+             {
+                  var badstr = 'Sorry phone area code must be between 000 and 999';
+                  res.render('pages/result', {myresults: badstr} );
+             }
+             else 
+             {
+                 console.log("typeof email = " + typeof email);
+                 var emailstr = JSON.stringify(email);
+                 console.log("emailstr = " + emailstr);
+               
+                 var first_at = emailstr.indexOf("@");
+                 var last_at = emailstr.lastIndexOf("@");
+               
+                 console.log("first_at = " + first_at);
+                 console.log("last_at = " + last_at);
+
+                 if (first_at == -1)
+                 {
+                      var badstr = 'Sorry email must have 1 @ character, for example darthvader@gmail.com';
+                      res.render('pages/result', {myresults: badstr} );
+                 }
+                 else if (first_at != last_at)
+                 {
+                      var badstr = 'Sorry email must have only 1 @ character, for example darthvader@gmail.com but you put more than 1 @ character';
+                      res.render('pages/result', {myresults: badstr} );
+                 }
+                 else
+                      connectAndInsert(); 
+             }
+          }//good
+      })//form.parse
   }
 };

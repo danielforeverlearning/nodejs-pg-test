6b0ee1fe7791479fa8311b0058b5485c26721413
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-09 18:49:42 -1000

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ express()
   
   .get('/', (req, res) => res.render('pages/home'))
 
-  .get('/exampledialog', (req, res) => res.render('pages/exampledialog'))
+  .get('/exampledialog', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'exampledialog.html')); })
 
   .get('/studenttableread', (req,res) => { studenttable.studenttablereadfunc(req,res); })
   .get('/tableread', (req,res) => {

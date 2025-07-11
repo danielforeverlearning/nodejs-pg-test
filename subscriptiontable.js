e6238e559cca0f7dd3f494c74cc73bb1774c6eec
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-10 18:11:54 -1000

    Update subscriptiontable.js

--- subscriptiontable.js
+++ subscriptiontable.js
@@ -18,21 +18,21 @@ const connectobj   = {
 
 module.exports = {
 
-    subscriptiontableread: function(req,res)  {
+    subscriptiontablereadfunc: function(req,res)  {
             async function connectAndRead() {  
                         const client       = new Client(connectobj);
                         try {
                           await client.connect();
-                          console.log('subscriptiontableread Connected to PostgreSQL!');
+                          console.log('subscriptiontablereadfunc Connected to PostgreSQL!');
                           const result = await client.query('SELECT * FROM subscription');
                           console.log("result = " + JSON.stringify(result));
                           res.render('pages/subscriptiontableread', {results: result.rows} );
                         } catch (err) {
-                            var result = 'subscriptiontableread ERROR = ' + err;
+                            var result = 'subscriptiontablereadfunc ERROR = ' + err;
                             res.send(result);
                         } finally {
                             await client.end();
-                            console.log('subscriptiontableread Disconnected from PostgreSQL.');
+                            console.log('subscriptiontablereadfunc Disconnected from PostgreSQL.');
                         }
             }
             connectAndRead(); 

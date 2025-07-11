64dc2b0b5c1ff7f52c3adac50265ddb48e6a4905
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-10 19:39:00 -1000

    Update index.js

--- index.js
+++ index.js
@@ -97,29 +97,7 @@ express()
   })
 
   .get('/dbcreatestudenttable', (req, res) => { studenttable.studenttablecreatefunc(req, res); })
-  .get('/dbcreatetable', (req, res) => {
-          async function connectAndCreate() {
-                      const client       = new Client(connectobj);
-                      try {
-                          await client.connect();
-                          console.log('Connected to PostgreSQL!');
-              
-                          // Example: create table
-                          const createRes = await client.query(
-                              'CREATE TABLE cars (ID SERIAL PRIMARY KEY, BRAND VARCHAR(255),MODEL VARCHAR(255),YEAR INT);'
-                          );
-                          var result = 'createRes = ' + JSON.stringify(createRes);
-                          res.send(result);
-                      } catch (err) {
-                          var result = 'Error connecting or creating table = ' + err;
-                          res.send(result);
-                      } finally {
-                          await client.end();
-                          console.log('Disconnected from PostgreSQL.');
-                      }
-          }
-          connectAndCreate();
-  })
+  .get('/dbcreatesubscriptiontable', (req, res) => { subscriptiontable.subscriptiontablecreatefunc(req, res); })
 
   .get('/dbdropstudenttable', (req, res) => { studenttable.studenttabledropfunc(req, res); })
   .get('/dbdroptable', (req, res) => {

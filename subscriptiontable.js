a7d8780d5823065a7c6a8d2cb16cfbab6fc2972c
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-10 19:11:49 -1000

    Update subscriptiontable.js

--- subscriptiontable.js
+++ subscriptiontable.js
@@ -38,4 +38,45 @@ module.exports = {
             connectAndRead(); 
   },
 
+  subscriptiontableinsertsubmitfunc: function(req,res) {
+      var brand;
+      var model;
+      var year;
+      async function connectAndInsert() {    
+                        const client       = new Client(connectobj);
+                        try {
+                          await client.connect();
+                          console.log('INSERT INTO cars Connected to PostgreSQL!');
+                          var insertstmt = "INSERT INTO cars (BRAND, MODEL, YEAR) VALUES ('" + brand + "', '" + model + "', " + year + ");";
+                          console.log(insertstmt);
+                          const insertRes = await client.query(insertstmt);
+                          var resultstr = 'insertRes = ' + JSON.stringify(insertRes);
+                          res.render('pages/result', {myresults: resultstr} );
+                        } catch (err) {
+                            var badstr = 'INSERT INTO cars ERROR = ' + err;
+                            res.render('pages/result', {myresults: badstr} );
+                        } finally {
+                            await client.end();
+                            console.log('INSERT INTO cars Disconnected from PostgreSQL.');
+                        }
+      }
+    
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+  
+          if (err)
+          {
+             res.send("tableinsertsubmit err = " + err);
+          }
+          else
+          {
+             console.log("fields = " + JSON.stringify(fields) + "<br/>files = " + JSON.stringify(files));
+             brand = fields.brand_name;
+             model = fields.model_name;
+             year = fields.year_name;
+             connectAndInsert(); 
+          }
+      })
+  }//subscriptiontableinsertsubmitfunc
+
 };

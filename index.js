5db4ec2077a3d4485ad1631abcd0dc2e5b4aa7e5
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-10 19:07:45 -1000

    Update index.js

--- index.js
+++ index.js
@@ -55,50 +55,8 @@ express()
   .get('/studenttableinsert', (req, res) => res.render('pages/studenttableinsert'))
   .post('/studenttableinsertsubmit', (req, res) => { studenttable.studenttableinsertfunc(req,res); })
   .get('/subscriptiontableinsert', (req, res) => res.render('pages/subscriptiontableinsert'))
-  .post('/tableinsertsubmit', (req, res) => {
-      var brand;
-      var model;
-      var year;
-      async function connectAndInsert() {    
-                        const client       = new Client(connectobj);
-                        try {
-                          await client.connect();
-                          console.log('INSERT INTO cars Connected to PostgreSQL!');
-                          var insertstmt = "INSERT INTO cars (BRAND, MODEL, YEAR) VALUES ('" + brand + "', '" + model + "', " + year + ");";
-                          console.log(insertstmt);
-                          const insertRes = await client.query(insertstmt);
-                          var resultstr = 'insertRes = ' + JSON.stringify(insertRes);
-                          res.render('pages/result', {myresults: resultstr} );
-                        } catch (err) {
-                            var badstr = 'INSERT INTO cars ERROR = ' + err;
-                            res.render('pages/result', {myresults: badstr} );
-                        } finally {
-                            await client.end();
-                            console.log('INSERT INTO cars Disconnected from PostgreSQL.');
-                        }
-      }
-    
-      var form = new formidable.IncomingForm();
-      form.parse(req, function (err, fields, files) {
+  .post('/subscriptiontableinsertsubmit', (req, res) => { subscriptiontable.subscriptiontableinsertsubmitfunc(req,res); })
   
-          if (err)
-          {
-             res.send("tableinsertsubmit err = " + err);
-          }
-          else
-          {
-             console.log("fields = " + JSON.stringify(fields) + "<br/>files = " + JSON.stringify(files));
-             brand = fields.brand_name;
-             model = fields.model_name;
-             year = fields.year_name;
-             connectAndInsert(); 
-          }
-      })
-    
-  })
-
-    
-
 
   .get('/studenttabledelete', (req, res) => res.render('pages/studenttabledelete'))
   .post('/studenttabledeletesubmit', (req, res) => { studenttable.studenttabledeletefunc(req,res); })

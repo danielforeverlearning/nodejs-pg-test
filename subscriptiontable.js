5d002ac75b0a7e3950b8158589fdf0a7df9721ab
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-12 00:56:21 -1000

    Update subscriptiontable.js

--- subscriptiontable.js
+++ subscriptiontable.js
@@ -8,13 +8,12 @@ const PORT         = process.env.PORT || 5000
 const { Client }   = require('pg');
 
 const connectobj   = {
-                                    user: 'dbadmin', // e.g., 'postgres'
+                                    user: 'garagedb_i5vo_user', // e.g., 'postgres'
                                     host: 'dpg-d1p3n83ipnbc73fk1qk0-a',
                                     database: 'garagedb_i5vo', // The database you created
                                     password: 'TTMNPyqcxBv2Gf3WwmBhAlnsLx3M40ek',
                                     port: 5432,
                      };
-
 
 module.exports = {
 

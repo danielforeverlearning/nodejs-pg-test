ff7c0dac337a7922174f9924df6bd8bd2ddd40a2
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-12 00:55:39 -1000

    Update studenttable.js

--- studenttable.js
+++ studenttable.js
@@ -9,7 +9,7 @@ const { Client }   = require('pg');
 
 
 const connectobj   = {
-                                    user: 'dbadmin', // e.g., 'postgres'
+                                    user: 'garagedb_i5vo_user', // e.g., 'postgres'
                                     host: 'dpg-d1p3n83ipnbc73fk1qk0-a',
                                     database: 'garagedb_i5vo', // The database you created
                                     password: 'TTMNPyqcxBv2Gf3WwmBhAlnsLx3M40ek',

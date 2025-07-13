58bdb4888d45f50c38e3bd2c2ace6f1904e03ce3
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-12 20:48:16 -1000

    Update studenttable.js

--- studenttable.js
+++ studenttable.js
@@ -192,54 +192,6 @@ module.exports = {
   },
 
 
-  studenttablecreatefunc: function(req, res) {
-          async function connectAndCreate() {
-                      const client       = new Client(connectobj);
-                      try {
-                          await client.connect();
-                          console.log('Connected to PostgreSQL!');
-              
-                          // Example: create table
-                          const createRes = await client.query(
-                              'CREATE TABLE student (ID SERIAL PRIMARY KEY, FIRSTNAME VARCHAR(255), LASTNAME VARCHAR(255), EMAIL VARCHAR(255), PHONEAREACODE INT, PHONENUMBER INT);'
-                          );
-                          var result = 'createRes = ' + JSON.stringify(createRes);
-                          res.send(result);
-                      } catch (err) {
-                          var result = 'Error connecting or creating table = ' + err;
-                          res.send(result);
-                      } finally {
-                          await client.end();
-                          console.log('Disconnected from PostgreSQL.');
-                      }
-          }
-          connectAndCreate();
-  },
-
-  studenttabledropfunc: function(req, res) {
-          const client       = new Client(connectobj);
-          async function connectAndDrop() {
-                      try {
-                          await client.connect();
-                          console.log('Connected to PostgreSQL!');
-              
-                          // Example: drop table
-                          const dropRes = await client.query(
-                              'DROP TABLE student;'
-                          );
-                          var result = 'dropRes = ' + JSON.stringify(dropRes);
-                          res.send(result);
-                      } catch (err) {
-                          var result = 'Error connecting or dropping table = ' + err;
-                          res.send(result);
-                      } finally {
-                          await client.end();
-                          console.log('Disconnected from PostgreSQL.');
-                      }
-          }
-          connectAndDrop();
-  },
-
   updatefunc: function(req, res, studentID) {
     
       async function connectAndSelectByID() {  

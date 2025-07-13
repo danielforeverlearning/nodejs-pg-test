c0b245f596fa36c5dc282a27056c8a9fdda32206
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-13 12:44:31 -1000

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ express()
   .get('/studenttableinsert', (req, res) => res.render('pages/studenttableinsert'))
   .post('/studenttableinsertsubmit', (req, res) => { studenttable.studenttableinsertfunc(req,res); })
  
-  .get('/studentdeleteconfirm/:id', (req,res) => res.render('pages/delete_confirm_diag', {studentID: req.params.id}))
+  .get('/studentdeleteconfirm/:id/:firstname/:lastname', (req,res) => res.render('pages/delete_confirm_diag', {studentID: req.params.id, firstname: req.params.firstname, lastname: req.params.lastname}))
   .get('/studentdelete/:id', (req, res) => {  
                                               const studentID = req.params.id;
                                               studenttable.deletefunc(req,res,studentID);

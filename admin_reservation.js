e5afc88c1bd6f3b264ec84b97f0a0288deaf30a2
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-14 11:19:44 -1000

    Update admin_reservation.js

--- admin_reservation.js
+++ admin_reservation.js
@@ -8,7 +8,7 @@ const PORT         = process.env.PORT || 5000
 const { Client }   = require('pg');
 
 var db_credential  = require('./db_credential');
-const connectobj   = db_credential.connectobj;
+const connectobj   = db_credential.myconnectobj();
 /*****
 const connectobj   = {
                                     user: , // e.g., 'postgres'

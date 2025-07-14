2604104a6b803cbce7a8f369117b59347ea2e3a8
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-14 11:08:13 -1000

    Create db_credential.js

---
+++ db_credential.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+const connectobj   = {
+                                    user: 'garagedb_i5vo_user', // e.g., 'postgres'
+                                    host: 'dpg-d1p3n83ipnbc73fk1qk0-a',
+                                    database: 'garagedb_i5vo', // The database you created
+                                    password: 'TTMNPyqcxBv2Gf3WwmBhAlnsLx3M40ek',
+                                    port: 5432,
+                     };
+};

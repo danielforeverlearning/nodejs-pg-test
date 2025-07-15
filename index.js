695a7a19d6b06aa13274d596407598d5e306535f
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-15 13:52:33 -1000

    Update index.js

--- index.js
+++ index.js
@@ -78,5 +78,10 @@ express()
 
   .get('/dbcreatereservationtable', (req, res) => { admin_reservation.reservationtablecreatefunc(req, res); })
   .get('/dbdropreservationtable', (req, res) => { admin_reservation.reservationtabledropfunc(req, res); })
+  .get('/reservation_month_view', (req, res) => {
+                                                     const myDate = new Date("2025-08-01"); // Creates a Date object for July 1, 2025
+                                                     const dayIndex = myDate.getDay(); //0=sunday, 1=monday, 2=tuesday, 3=wednesday, 4=thursday, 5=friday, 6=saturday
+                                                     res.render('pages/reservation_month_view', {month: 8, year: 2025, firstday: dayIndex});
+                                                })
     
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))

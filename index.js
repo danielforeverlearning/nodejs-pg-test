7e137e21c81483c40a7a226b3b7f39011c79c3f2
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-20 05:12:54 -1000

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,12 @@ express()
                                               const studentID = req.params.id;
                                               studenttable.updatefunc(req,res,studentID);
                                            })
-  .get('/student', (req,res) => { studenttable.studentviewfunc(req,res); })
+  .get('/student/:sortid/:sortfirstname/:sortlastname', (req,res) => {  
+                                                                        const sortID = req.params.sortid;
+                                                                        const sortfirstname = req.params.sortfirstname;
+                                                                        const sortlastname  = req.params.sortlastname;
+                                                                        studenttable.studentviewfunc(req,res,sortID,sortfirstname,sortlastname);
+                                                                     })
   .get('/studentmonthlysubscription/:id/:firstname/:lastname', (req,res) => {  
                                                             const studentID = req.params.id;
                                                             const firstname = req.params.firstname;

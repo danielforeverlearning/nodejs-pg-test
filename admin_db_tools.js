a9bb42f25b034ff2c6684b2a832f303d9b781adb
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-22 17:51:10 -1000

    Update admin_db_tools.js

--- admin_db_tools.js
+++ admin_db_tools.js
@@ -76,6 +76,7 @@ module.exports = {
                   else
                       console.log(fileName + ' deleted ssuccessfully');
               });
+              res.redirect('/');
             }
         });
   }

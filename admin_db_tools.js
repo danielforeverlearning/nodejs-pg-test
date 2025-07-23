d0e79ee477bcf160c72d2dad427f94d7800999c9
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-22 21:59:26 -1000

    Update admin_db_tools.js

--- admin_db_tools.js
+++ admin_db_tools.js
@@ -23,13 +23,42 @@ const connectobj   = {
 var fileName;
 var filePath;
 
-function make_student_table_func() {
-          var returnobj;
-          async function connectAndRead() {
+module.exports = {  
+
+  download_student_table_func: function(req,res) {
+        res.download(filePath, fileName, (err) => {
+            if (err) {
+              console.error('Error downloading the file:', err);
+              res.status(500).send('Error downloading the file:' + err);
+            } else {
+              fs.unlink(filePath, (err) => {
+                  if (err)
+                      console.error('Error deleting file:', err);
+                  else
+                      console.log(fileName + ' deleted ssuccessfully');
+              });
+            }
+        });
+  },
+
+  async function connectAndRead() {
+                      var returnobj;
+                      try {
+                          var dd = new Date();
+                          fileName =  'student_table_' + dd.getFullYear() + '_' + (dd.getMonth() + 1) + '_' + dd.getDate() + '_' + dd.getHours() + '_' + dd.getMinutes() + '_' + dd.getSeconds() + '_' + dd.getMilliseconds() + '.csv';
+                          filePath = path.join(__dirname, 'public/' + fileName); // Path to your text file
+                          fs.writeFileSync(filePath, 'ID, FIRSTNAME, LASTNAME, EMAIL, PHONEAREACODE, PHONENUMBER\n');
+                      } catch (err) {
+                          var badstr = 'Error student table writeFileSync:' + err;
+                          returnobj = {status: -1, myresults: badstr};
+                          return returnobj;
+                      }
+    
                       const client       = new Client(connectobj);
                       try {
                           await client.connect();
                           const result = await client.query('SELECT * FROM student ORDER BY ID ASC');
+                          
                           result.rows.forEach(function(row) {
                                 try {
                                     var line = '' + row.id + ', "' + row.firstname + '", "' + row.lastname + '", "' + row.email + '", ' + row.phoneareacode + ', ' + row.phonenumber + '\n';
@@ -49,42 +78,11 @@ function make_student_table_func() {
                           returnobj = {status: 0, myresults: ""};
                           return returnobj;
                       }
-          }
-
-          try {
-              var dd = new Date();
-              fileName =  'student_table_' + dd.getFullYear() + '_' + (dd.getMonth() + 1) + '_' + dd.getDate() + '_' + dd.getHours() + '_' + dd.getMinutes() + '_' + dd.getSeconds() + '_' + dd.getMilliseconds() + '.csv';
-              filePath = path.join(__dirname, 'public/' + fileName); // Path to your text file
-              fs.writeFileSync(filePath, 'ID, FIRSTNAME, LASTNAME, EMAIL, PHONEAREACODE, PHONENUMBER\n');
-          } catch (err) {
-              var badstr = 'Error student table writeFileSync:' + err;
-              returnobj = {status: -1, myresults: badstr};
-              return returnobj;
-          }
-    
-          connectAndRead();
-} //make_student_table_func
-
-module.exports = {  
-
-  download_student_table_func: function(req,res) {
-        res.download(filePath, fileName, (err) => {
-            if (err) {
-              console.error('Error downloading the file:', err);
-              res.status(500).send('Error downloading the file:' + err);
-            } else {
-              fs.unlink(filePath, (err) => {
-                  if (err)
-                      console.error('Error deleting file:', err);
-                  else
-                      console.log(fileName + ' deleted ssuccessfully');
-              });
-            }
-        });
-  },
+  },  //connectAndRead
 
   make_all_db_table_files: function(req,res) {
-          var table_student = make_student_table_func();
+    
+          var table_student = connectAndRead();
           console.log("table_student = " + JSON.stringify(table_student));
           if (table_student.status == -1)
               res.render('pages/result', {myresults: table_student.myresults} );

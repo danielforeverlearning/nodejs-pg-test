9fb9862b921a9030d740b6cfd806c8eac4050d9b
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-30 13:55:24 -1000

    Update admin_student.js

--- admin_student.js
+++ admin_student.js
@@ -70,50 +70,241 @@ module.exports = {
 
   studenttablecreatefunc: function(req, res) {
           async function connectAndCreate() {
+                      var result;
                       const client       = new Client(connectobj);
                       try {
                           await client.connect();
-                          console.log('Connected to PostgreSQL!');
               
                           // Example: create table
                           const createRes = await client.query(
                               'CREATE TABLE student (ID SERIAL PRIMARY KEY, FIRSTNAME VARCHAR(255), LASTNAME VARCHAR(255), EMAIL VARCHAR(255), PHONEAREACODE INT, PHONENUMBER INT);'
                           );
-                          var result = 'createRes = ' + JSON.stringify(createRes);
-                          res.send(result);
+                          result = 'createRes = ' + JSON.stringify(createRes);
                       } catch (err) {
-                          var result = 'Error connecting or creating table = ' + err;
-                          res.send(result);
+                          result = 'Error connecting or creating table = ' + err;
                       } finally {
                           await client.end();
-                          console.log('Disconnected from PostgreSQL.');
+                          res.send(result);
                       }
           }
           connectAndCreate();
   },
 
   studenttabledropfunc: function(req, res) {
+          var result;
           const client       = new Client(connectobj);
           async function connectAndDrop() {
                       try {
                           await client.connect();
-                          console.log('Connected to PostgreSQL!');
               
                           // Example: drop table
                           const dropRes = await client.query(
                               'DROP TABLE student;'
                           );
-                          var result = 'dropRes = ' + JSON.stringify(dropRes);
-                          res.send(result);
+                          result = 'dropRes = ' + JSON.stringify(dropRes);
                       } catch (err) {
-                          var result = 'Error connecting or dropping table = ' + err;
-                          res.send(result);
+                          result = 'Error connecting or dropping table = ' + err;
                       } finally {
                           await client.end();
-                          console.log('Disconnected from PostgreSQL.');
+                          res.send(result);
                       }
           }
           connectAndDrop();
+  },
+
+  
+  studenttableinsertfunc: function(req, res)  {
+      var firstname;
+      var lastname;
+      var email;
+      var phoneareacode;
+      var phonenumber;
+      var resultstr;
+      async function connectAndInsert() {    
+                        const client       = new Client(connectobj);
+                        try {
+                          await client.connect();
+                          //console.log('INSERT INTO student Connected to PostgreSQL!');
+                          var insertstmt = "INSERT INTO student (FIRSTNAME, LASTNAME, EMAIL, PHONEAREACODE, PHONENUMBER) VALUES ('" + firstname + "', '" + lastname + "', '" + email + "', " + phoneareacode + ", " + phonenumber + ");";
+                          console.log(insertstmt);
+                          const insertRes = await client.query(insertstmt);
+                          resultstr = 'insertRes = ' + JSON.stringify(insertRes);
+                        } catch (err) {
+                            resultstr = 'INSERT INTO student ERROR = ' + err;
+                        } finally {
+                            await client.end();
+                            res.render('admin_pages/adminresult', {myresults: resultstr} );
+                        }
+      }
+    
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+  
+          if (err)
+          {
+             res.send("studenttableinsertfunc err = " + err);
+             return;
+          }
+          else
+          { //good
+             console.log("fields = " + JSON.stringify(fields) + "<br/>files = " + JSON.stringify(files));
+             firstname = fields.firstname_name;
+             lastname = fields.lastname_name;
+             email = fields.email_name;
+             phoneareacode = fields.phoneareacode_name;
+             phonenumber = fields.phonenumber_name;
+
+             //validation checking
+             if (phoneareacode < 0 || phoneareacode > 999)
+             {
+                  var badstr = 'Sorry phone area code must be between 000 and 999';
+                  res.render('admin_pages/adminresult', {myresults: badstr} );
+                  return;
+             }
+             else 
+             {
+                 console.log("typeof email = " + typeof email);
+                 var emailstr = JSON.stringify(email);
+                 console.log("emailstr = " + emailstr);
+               
+                 var first_at = emailstr.indexOf("@");
+                 var last_at = emailstr.lastIndexOf("@");
+               
+                 console.log("first_at = " + first_at);
+                 console.log("last_at = " + last_at);
+
+                 if (first_at == -1)
+                 {
+                      var badstr = 'Sorry email must have 1 @ character, for example darthvader@gmail.com';
+                      res.render('admin_pages/adminresult', {myresults: badstr} );
+                      return;
+                 }
+                 else if (first_at != last_at)
+                 {
+                      var badstr = 'Sorry email must have only 1 @ character, for example darthvader@gmail.com but you put more than 1 @ character';
+                      res.render('admin_pages/adminresult', {myresults: badstr} );
+                      return;
+                 }
+                 else
+                      connectAndInsert(); 
+             }
+          }//good
+      })//form.parse
+  },
+
+
+  updatefunc: function(req, res, studentID) {
+    
+      async function connectAndSelectByID() {  
+                        const client       = new Client(connectobj);
+                        try {
+                          await client.connect();
+                          const selectIDres = await client.query("SELECT * FROM student WHERE ID = " + studentID + ";");
+                          console.log("selectIDres = " + JSON.stringify(selectIDres));
+                          console.log("selectIDres.rows.length = " + selectIDres.rows.length);
+                          if (selectIDres.rows.length  != 1)
+                          {
+                              var badstr = 'Sorry there is no row in table student with ID = ' + studentID + ', if you want to update a row the ID must be good.';
+                              res.render('pages/result', {myresults: badstr} );
+                          }
+                          else
+                              res.render('pages/studenttableupdate2', {existingval: selectIDres.rows[0]} );
+                        } catch (err) {
+                              var badstr = 'updatefunc ID = ' + studentID + ', ERROR = ' + err;
+                              res.render('pages/result', {myresults: badstr} );
+                        } finally {
+                              await client.end();
+                              console.log('studenttableupdateIDfunc ID = ' + studentID + ', Disconnected from PostgreSQL.');
+                        }
+      }
+      connectAndSelectByID();
+  },
+
+
+  studenttableupdate3func: function(req, res) {
+      var primarykeyID;
+      var firstname;
+      var lastname;
+      var email;
+      var phoneareacode;
+      var phonenumber;
+      var dbgoodresult;
+      var badstr;
+      var updateRes;
+      async function connectAndUpdate() {    
+                        const client       = new Client(connectobj);
+                        try {
+                          await client.connect();
+                          //console.log('studenttableupdate3func Connected to PostgreSQL!');
+                          var stmt = "UPDATE student SET FIRSTNAME = '" + firstname + "', LASTNAME = '" + lastname + "', EMAIL = '" + email + "', PHONEAREACODE = " + phoneareacode + ", PHONENUMBER = " + phonenumber + " WHERE ID = " + primarykeyID + ";";
+                          console.log(stmt);
+                          updateRes = await client.query(stmt);
+                          dbgoodresult = true;
+                        } catch (err) {
+                            badstr = 'UPDATE student ID = ' + primarykeyID + ', err = ' + err;
+                            dbgoodresult = false;
+                        } finally {
+                            await client.end();
+                            //console.log('studenttableupdate3func Disconnected from PostgreSQL.');
+                            if (dbgoodresult) {
+                                 var resultstr = 'updateRes = ' + JSON.stringify(updateRes);
+                                 res.render('pages/result', {myresults: resultstr} );
+                            }
+                            else
+                                 res.render('pages/result', {myresults: badstr} );
+                        }
+      }
+    
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+  
+          if (err)
+          {
+             res.send("studenttableupdate3func err = " + err);
+          }
+          else
+          { //good
+             console.log("fields = " + JSON.stringify(fields) + "<br/>files = " + JSON.stringify(files));
+             primarykeyID = fields.primarykey_name;
+             firstname = fields.firstname_name;
+             lastname = fields.lastname_name;
+             email = fields.email_name;
+             phoneareacode = fields.phoneareacode_name;
+             phonenumber = fields.phonenumber_name;
+
+             //validation checking
+             if (phoneareacode < 0 || phoneareacode > 999)
+             {
+                  var badstr = 'Sorry phone area code must be between 000 and 999';
+                  res.render('pages/result', {myresults: badstr} );
+             }
+             else 
+             {
+                 console.log("typeof email = " + typeof email);
+                 var emailstr = JSON.stringify(email);
+                 console.log("emailstr = " + emailstr);
+               
+                 var first_at = emailstr.indexOf("@");
+                 var last_at = emailstr.lastIndexOf("@");
+               
+                 console.log("first_at = " + first_at);
+                 console.log("last_at = " + last_at);
+
+                 if (first_at == -1)
+                 {
+                      var badstr = 'Sorry email must have 1 @ character, for example darthvader@gmail.com';
+                      res.render('pages/result', {myresults: badstr} );
+                 }
+                 else if (first_at != last_at)
+                 {
+                      var badstr = 'Sorry email must have only 1 @ character, for example darthvader@gmail.com but you put more than 1 @ character';
+                      res.render('pages/result', {myresults: badstr} );
+                 }
+                 else
+                      connectAndUpdate(); 
+             }
+          }//good
+      })//form.parse
   }
 }; //module.exports
 

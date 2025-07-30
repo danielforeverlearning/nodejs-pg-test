9ee56dc12c11ea76c6ba556d7ec29e41f210cf3a
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-30 12:19:50 -1000

    Update admin_accounts.js

--- admin_accounts.js
+++ admin_accounts.js
@@ -11,6 +11,13 @@ var db_credential   = require('./db_credential');
 const connectobj    = db_credential.myconnectobj();
 
 
+function hashHmacJs(algo, data, key, raw_output = false) {
+    const hmac = crypto.createHmac(algo, key);
+    hmac.update(data);
+    const digest = hmac.digest(raw_output ? 'binary' : 'hex');
+    return digest;
+}//hashHmacJs
+
 module.exports = {
 
   studentacctcreatefunc: function(req,res) {
@@ -100,7 +107,93 @@ module.exports = {
                       }
           }
           connectAndDrop();
-  }//adminacctdropfunc
+  }, //adminacctdropfunc
+
+  
+  
+securityReadByStudentID: async function(res, studentID, firstname, lastname) {  
+                        var dbgoodresult;
+                        var badstr;
+                        var result;
+                        const client       = new Client(connectobj);
+                        try {
+                          await client.connect();
+
+                          var selectstmt = 'SELECT * FROM security WHERE STUDENTID = ' + studentID;
+                          result = await client.query(selectstmt);
+                          console.log(" result = " + JSON.stringify(result));
+                          dbgoodresult = true;
+                        } catch (err) {
+                            dbgoodresult = false;
+                            badstr = "securityReadByStudentID err = " + err;
+                        } finally {
+                            await client.end();
+                            if (dbgoodresult) {
+                                console.log("securityReadByStudentID result.rows.length = " + result.rows.length);
+                                res.render('pages/securitytableinsert', {rows: result.rows, rowslength: result.rows.length, studentID: studentID, firstname: firstname, lastname: lastname});
+                            }
+                            else
+                              res.render('pages/result', {myresults: badstr} );
+                        }
+  }, //securityReadByStudentID
+  
+  securitytableinsertfunc: function(req, res)  {
+      var password;
+      var confirm;
+      var studentID;
+      var passwordhash;
+      var resultstr;
+      async function connectAndInsert() {    
+                        const client       = new Client(connectobj);
+                        try {
+                          await client.connect();
+                          var insertstmt = "INSERT INTO security (STUDENTID, PASSWORDHASH) VALUES (" + studentID + ", '" + passwordhash + "');";
+                          console.log(insertstmt);
+                          const insertRes = await client.query(insertstmt);
+                          resultstr = 'insertRes = ' + JSON.stringify(insertRes);
+                        } catch (err) {
+                            resultstr = 'INSERT INTO security ERROR = ' + err;
+                        } finally {
+                            await client.end();
+                            res.render('pages/result', {myresults: resultstr} );
+                        }
+      }
+    
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+  
+          if (err)
+          {
+             res.send("securitytableinsertfunc err = " + err);
+          }
+          else
+          { //good
+             console.log("fields = " + JSON.stringify(fields));
+             console.log("files = " + JSON.stringify(files));
+              
+             password = fields.password_name[0];
+             confirm = fields.confirm_name[0];
+             studentID = fields.studentID_name[0];
+             console.log("typeof password = " + typeof password);
+             console.log("typeof confirm = " + typeof confirm);
+             console.log("typeof studentID = " + typeof studentID);
+             console.log('password="' + password + '" confirm="' + confirm + '" studentID=' + studentID);
+             
+             //validation checking
+             if (password === confirm)
+             {
+                  passwordhash = hashHmacJs('sha256', password, 'nodejs-pg-test');
+                  console.log("passwordhash = " + passwordhash);
+                  connectAndInsert();
+             }
+             else
+             {
+                  var badstr = 'Sorry password and confirm must be exactly the same: ' + 'password="' + password + '" confirm="' + confirm + '"';
+                  res.render('pages/result', {myresults: badstr} );
+             }
+          }//good
+      })//form.parse
+  } //securitytableinsertfunc
   
   
 }; //module.exports

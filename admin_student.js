5fa2f95b7b308e203899743895881ed45ee31926
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-30 00:51:34 -1000

    Update admin_student.js

--- admin_student.js
+++ admin_student.js
@@ -13,6 +13,58 @@ const connectobj    = db_credential.myconnectobj();
 
 module.exports = {
 
+  studentviewfunc: function(req,res,sortorder)  {
+            async function connectAndRead() {  
+                        const client       = new Client(connectobj);
+                        try {
+                          await client.connect();
+
+                          var selectstmt = 'SELECT * FROM student';
+                          if (sortorder == 100)
+                               selectstmt += ' ORDER BY ID ASC';
+                          else if (sortorder == 200)
+                               selectstmt += ' ORDER BY ID DESC';
+                          else if (sortorder == 10)
+                               selectstmt += ' ORDER BY FIRSTNAME ASC';
+                          else if (sortorder == 20)
+                               selectstmt += ' ORDER BY FIRSTNAME DESC';
+                          else if (sortorder == 1)
+                               selectstmt += ' ORDER BY LASTNAME ASC';
+                          else if (sortorder == 2)
+                               selectstmt += ' ORDER BY LASTNAME DESC';
+                          
+                          const result = await client.query(selectstmt);
+                          //console.log(" result = " + JSON.stringify(result));
+                          res.render('admin_pages/studentview', {results: result.rows, sortorder: sortorder} );
+                        } catch (err) {
+                            var errormsg = " err = " + err;
+                            //console.log(errormsg);
+                            res.render('pages/result', {myresults: errormsg} );
+                        } finally {
+                            await client.end();
+                        }
+            }
+            connectAndRead(); 
+  },
+
+  deletefunc: function(req, res, studentID) {
+      async function connectAndDelete() {  
+                        const client       = new Client(connectobj);
+                        try {
+                          await client.connect();
+                          const deleteRes = await client.query("DELETE FROM subscription WHERE STUDENTID = " + studentID + "; DELETE FROM student WHERE ID = " + studentID + ";");
+                          var resultstr = 'student subscription deletefunc = ' + JSON.stringify(deleteRes);
+                          res.render('pages/result', {myresults: resultstr} );
+                        } catch (err) {
+                            var badstr = 'student subscription deletefunc, ERROR = ' + err;
+                            res.render('pages/result', {myresults: badstr} );
+                        } finally {
+                            await client.end();
+                        }
+      }
+      connectAndDelete(); 
+  },
+
   studenttablecreatefunc: function(req, res) {
           async function connectAndCreate() {
                       const client       = new Client(connectobj);

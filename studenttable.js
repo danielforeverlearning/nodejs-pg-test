5ddbd0397e15421966d36cb72189173a0588d7fe
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-30 00:12:30 -1000

    Update studenttable.js

--- studenttable.js
+++ studenttable.js
@@ -196,9 +196,8 @@ module.exports = {
                             await client.end();
                             //console.log('studenttableupdate3func Disconnected from PostgreSQL.');
                             if (dbgoodresult) {
-                                 //var resultstr = 'updateRes = ' + JSON.stringify(updateRes);
-                                 //res.render('pages/result', {myresults: resultstr} );
-                                 securitytable.securityReadByStudentID(res, primarykeyID, firstname, lastname);
+                                 var resultstr = 'updateRes = ' + JSON.stringify(updateRes);
+                                 res.render('pages/result', {myresults: resultstr} );
                             }
                             else
                                  res.render('pages/result', {myresults: badstr} );

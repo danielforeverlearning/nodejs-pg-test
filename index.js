76b54774e238e23e85397bfe1fe157bb7bf53954
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-31 21:52:02 -1000

    Update index.js

--- index.js
+++ index.js
@@ -32,11 +32,6 @@ express()
 
   .get('/', (req, res) => res.send("please come back a couple hours from now under construction, time starting construction 12:26AM 7/30/2025 HST"))
 
-  .post('/adminoverwritestudacctsubmit/:id', (req, res) => { 
-        const studentID = req.params.id;
-        admin_accounts.adminoverwritestudacctsubmitfunc(req,res,studentID);
-  })
-
 
   .get('/adminhome', (req, res) => res.render('admin_pages/adminhome'))
   .get('/student/:sortorder', (req,res) => {  
@@ -58,6 +53,10 @@ express()
   .get('/studentupdate/:id', (req, res) => {  
         const studentID = req.params.id;
         admin_student.updatefunc(req,res,studentID);
+  })
+  .post('/adminoverwritestudacctsubmit/:id', (req, res) => { 
+        const studentID = req.params.id;
+        admin_accounts.adminoverwritestudacctsubmitfunc(req,res,studentID);
   })
   .get('/student/:sortorder', (req,res) => {  
         const sortorder = req.params.sortorder;

6b01e8e1985ab859fe109e12118dc03ae280191a
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-31 23:55:57 -1000

    Update account_student.js

--- account_student.js
+++ account_student.js
@@ -31,11 +31,22 @@ module.exports = {
       async function querydb() {    
                         const client       = new Client(connectobj);
                         try {
-                          await client.connect();
-                          var stmt  = "SELECT account_student WHERE EMAIL=" + email + ";";
-                          console.log(stmt);
-                          query_result = await client.query(stmt);
-                          goodquery = true;
+                              await client.connect();
+                              var stmt  = "SELECT account_student WHERE EMAIL=" + email + ";";
+                              console.log(stmt);
+                              query_result = await client.query(stmt);
+                              console.log("query_result = " + JSON.stringify(query_result));
+                              console.log("query_result.rows.length = " + query_result.rows.length);
+                              if (query_result.rows.length == 0) {
+                                   badstr = 'Sorry there is no student account with email = ' + email;
+                                   goodquery = false;
+                              } 
+                              else if (query_result.rows.length > 1) {
+                                   badstr = 'wow ok, more than 1 student account found with email = ' + email + ', that means i need to add validation code when creating student accounts on insert and update, need to repair code.';
+                                   goodquery = false;
+                              }
+                              else
+                                   goodquery = true;
                         } catch (err) {
                             goodquery = false;
                             badstr = 'loginstudentsubmitfunc ERROR = ' + err;
@@ -43,6 +54,11 @@ module.exports = {
                             await client.end();
                             if (goodquery)
                             {
+                                 var checkpasswordhash = hashHmacJs('shs256', email, 'nodejs-pg-test');
+                                 if (checkpasswordhash === query_result.rows[0].passwordhash)
+                                      res.render('pages/result', {myresults: 'good password'} );
+                                 else
+                                      res.render('pages/result', {myresults: 'bad password'} );
                             }
                             else
                                  res.render('pages/result', {myresults: badstr} );
@@ -51,7 +67,6 @@ module.exports = {
     
       var form = new formidable.IncomingForm();
       form.parse(req, function (err, fields, files) {
-  
           if (err)
           {
              res.send("loginstudentsubmitfunc form.parse ERROR = " + err);
@@ -63,7 +78,7 @@ module.exports = {
              console.log("files = " + JSON.stringify(files));
               
              password = fields.password_name[0];
-             email = fields.email_name[0];
+             email = fields.email_name[0].toLowerCase();
              
              querydb();
           }//good

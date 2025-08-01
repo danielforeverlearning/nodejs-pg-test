76a0ac2e914fa53f20b2d2a4d4f45708fe80d625
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-07-31 22:36:17 -1000

    Update admin_accounts.js

--- admin_accounts.js
+++ admin_accounts.js
@@ -2,6 +2,7 @@ const express      = require('express')
 const path         = require('path')
 var   https        = require('https');
 const formidable   = require('formidable')
+const crypto       = require('crypto');
 
 const PORT         = process.env.PORT || 5000
 

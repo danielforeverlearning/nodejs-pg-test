1c4354db9134d3ecdf92782537cc1e5f2d373ae6
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-08-01 01:56:52 -1000

    Update admin_student.js

--- admin_student.js
+++ admin_student.js
@@ -125,7 +125,7 @@ module.exports = {
                         //check duplicate email
                         try {
                              await client.connect();
-                             var query = "SELECT * FROM student WHERE EMAIL='" + email + "' AND ID <> " + primarykeyID + ";";
+                             var query = "SELECT * FROM student WHERE EMAIL='" + email + "';";
                              const queryRes = await client.query(query);
                              if (queryRes.rows.length > 0)
                              {

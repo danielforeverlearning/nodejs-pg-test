b8e0dc57264992653ef71f1f43e048805d4abf73
danielforeverlearning <12089490+danielforeverlearning@users.noreply.github.com>
2025-08-03 03:40:07 -1000

    Update admin_reservation.js

--- admin_reservation.js
+++ admin_reservation.js
@@ -167,28 +167,19 @@ module.exports = {
                    if (month <= 0 || month > 12)
                    {
                           var badstr = 'Sorry month must be between 1 and 12';
-                          if (adminbool)
-                               res.render('admin_pages/adminresult', {myresults: badstr} );
-                          else
-                               res.render('pages/result', {myresults: badstr} )
+                          res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                           return;
                    }
                    else if (year < currentYear)
                    {
                           var badstr = 'Sorry year must be ' + currentYear + ' or greater';
-                          if (adminbool)
-                               res.render('admin_pages/adminresult', {myresults: badstr} );
-                          else
-                               res.render('pages/result', {myresults: badstr} );
+                          res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                           return;
                    }
                    else if ((year == currentYear && month < currentMonth) || (year == currentYear && month == currentMonth && day < currentDay))
                    {
                           var badstr = 'Sorry, reservation must be made for today or later, today is year=' + currentYear + ' month=' + currentMonth + ' day=' + currentDay;
-                          if (adminbool)
-                               res.render('admin_pages/adminresult', {myresults: badstr} );
-                          else
-                               res.render('pages/result', {myresults: badstr} );
+                          res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                           return;
                    }
                    else if (month == 1)
@@ -196,10 +187,7 @@ module.exports = {
                                if (day < 0 || day > 31)
                                {
                                    var badstr = 'Sorry, since the month is January please enter a day between 1 and 31';
-                                   if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                                   else
-                                        res.render('pages/result', {myresults: badstr} );
+                                   res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                                    return;
                                }
                    }
@@ -210,10 +198,7 @@ module.exports = {
                                    if (day < 0 || day > 29)
                                    {
                                        var badstr = 'Sorry, since the year ' + year + ' is a leap year and the month is February, please enter a day between 1 and 29';
-                                       if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                                       else
-                                        res.render('pages/result', {myresults: badstr} );
+                                       res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                                        return;
                                    }   
                                }
@@ -222,10 +207,7 @@ module.exports = {
                                    if (day < 0 || day > 28)
                                    {
                                        var badstr = 'Sorry, since the year ' + year + ' is NOT a leap year and the month is February, please enter a day between 1 and 28';
-                                       if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                                       else
-                                        res.render('pages/result', {myresults: badstr} );
+                                       res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                                        return;
                                    }  
                                }
@@ -235,10 +217,7 @@ module.exports = {
                                if (day < 0 || day > 31)
                                {
                                    var badstr = 'Sorry, since the month is March please enter a day between 1 and 31';
-                                   if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                                   else
-                                        res.render('pages/result', {myresults: badstr} );
+                                   res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                                    return;
                                }
                    }
@@ -247,10 +226,7 @@ module.exports = {
                                if (day < 0 || day > 30)
                                {
                                    var badstr = 'Sorry, since the month is April please enter a day between 1 and 30';
-                                   if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                                   else
-                                        res.render('pages/result', {myresults: badstr} );
+                                   res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                                    return;
                                }
                    }
@@ -259,10 +235,7 @@ module.exports = {
                          if (day < 0 || day > 31)
                          {
                              var badstr = 'Sorry, since the month is May please enter a day between 1 and 31';
-                             if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                             else
-                                        res.render('pages/result', {myresults: badstr} );
+                             res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                              return;
                          }
                    }
@@ -271,10 +244,7 @@ module.exports = {
                          if (day < 0 || day > 30)
                          {
                              var badstr = 'Sorry, since the month is June please enter a day between 1 and 30';
-                             if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                             else
-                                        res.render('pages/result', {myresults: badstr} );
+                             res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                              return;
                          }
                    }
@@ -283,10 +253,7 @@ module.exports = {
                                if (day < 0 || day > 31)
                                {
                                    var badstr = 'Sorry, since the month is July please enter a day between 1 and 31';
-                                   if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                                   else
-                                        res.render('pages/result', {myresults: badstr} );
+                                   res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                                    return;
                                }
                    }
@@ -295,10 +262,7 @@ module.exports = {
                                if (day < 0 || day > 31)
                                {
                                    var badstr = 'Sorry, since the month is August please enter a day between 1 and 31';
-                                   if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                                   else
-                                        res.render('pages/result', {myresults: badstr} );
+                                   res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                                    return;
                                }
                    }
@@ -307,10 +271,7 @@ module.exports = {
                                if (day < 0 || day > 30)
                                {
                                    var badstr = 'Sorry, since the month is September please enter a day between 1 and 30';
-                                   if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                                   else
-                                        res.render('pages/result', {myresults: badstr} );
+                                   res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                                    return;
                                }
                    }
@@ -319,10 +280,7 @@ module.exports = {
                                if (day < 0 || day > 31)
                                {
                                    var badstr = 'Sorry, since the month is October please enter a day between 1 and 31';
-                                   if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                                   else
-                                        res.render('pages/result', {myresults: badstr} );
+                                   res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                                    return;
                                }
                    }
@@ -331,10 +289,7 @@ module.exports = {
                                if (day < 0 || day > 30)
                                {
                                    var badstr = 'Sorry, since the month is November please enter a day between 1 and 30';
-                                   if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                                   else
-                                        res.render('pages/result', {myresults: badstr} );
+                                   res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                                    return;
                                }
                    }
@@ -343,10 +298,7 @@ module.exports = {
                                if (day < 0 || day > 31)
                                {
                                    var badstr = 'Sorry, since the month is December please enter a day between 1 and 31';
-                                   if (adminbool)
-                                        res.render('admin_pages/adminresult', {myresults: badstr} );
-                                   else
-                                        res.render('pages/result', {myresults: badstr} );
+                                   res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                                    return;
                                }
                    }
@@ -371,20 +323,14 @@ module.exports = {
                           console.log(insertstmt);
                           const insertRes = await client.query(insertstmt);
                           var resultstr = 'reservation_check_location_time insertRes = ' + JSON.stringify(insertRes);
-                          if (adminbool)
-                               res.render('admin_pages/adminresult', {myresults: resultstr} );
-                          else
-                               res.render('pages/result', {myresults: resultstr} );
+                          res.render('admin_pages/admin_makereservation_success', {myresults: resultstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                         } catch (err) {
                             var badstr = 'reservation_check_location_time, ERROR = ' + err;
-                            if (adminbool)
-                                 res.render('admin_pages/adminresult', {myresults: badstr} );
-                            else
-                                 res.render('pages/result', {myresults: badstr} );
+                            res.render('admin_pages/admin_makereservation_result', {myresults: badstr, adminbool: adminbool, studentID: studentID, firstname: firstname, lastname:lastname} );
                         } finally {
                             await client.end();
                         }
-          }
+          } //connectAndInsert
 
     
                    if (timeint == 1000)
